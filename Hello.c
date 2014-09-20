9b15406f358fb65545431f166a115cdf49661e0d
DanielXuTong <1204897879@qq.com>
2014-09-20 23:42:42 +0800

    Hello this is the third time I sucessful

--- Hello.c
+++ Hello.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 {
-	printf("Hello this is the world of c");
-	
+    printf("welcome to the welcome of c ");
+    printf("nice to meet you ,daring");
+    printf("I am glad that I can do what I want at the moment");
     printf("Do you know how can I do it for your?");
 
 	
