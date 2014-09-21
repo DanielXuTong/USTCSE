bd978138cd19733600cce97c5d11fa998920bfb8
DanielXuTong <1204897879@qq.com>
2014-09-21 12:44:25 +0800

    6

--- Hello.c
+++ Hello.c
@@ -1,20 +1,21 @@
 /***************************************************************************/
 /*                                                                         */
-/*  Copyright (C)sse.ustc.edu.cn,	2014-2015                              */
-/*  FILE NAME				    :	menu.c                                 */
+/*  Copyright (C)sse.ustc.edu.cn,	2014-2015                          	   */
+/*  FILE NAME					:	menu.c                                 */
 /*  PRINCIPAL AUTHOR			:	Xutong                                 */
-/*  SUBSYSTEM NAME			    :	menu                                   */
-/*  MODULE NAME				    :	menu                                   */
-/*  LANGUAGE				    :	C                                      */
+/*  SUBSYSTEM NAME				:	menu                                   */
+/*  MODULE NAME					:	menu                                   */
+/*  LANGUAGE					:	C                                      */
 /*  TARGET ENVIROMENT			:	ANY                                    */
 /*  DATE OF FIRST RELEASE		:	2014/09/14                             */
-/*  DESCRIPTION				    :	This is a menu program                 */
+/*  DESCRIPTION					:	This is a menu program                 */
 /***************************************************************************/
 
 
 #include<stdio.h>
 #include<stdlib.h>
 {
+    	
 	
     printf("welcome to the welcome of c ");
     printf("nice to meet you ,daring");
