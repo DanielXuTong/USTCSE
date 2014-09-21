97fbe56e01fbf83236b0459bd8353e278c58ab62
DanielXuTong <1204897879@qq.com>
2014-09-21 12:47:07 +0800

    7

--- Hello.c
+++ Hello.c
@@ -15,12 +15,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 {
-    	
-	
     printf("welcome to the welcome of c ");
     printf("nice to meet you ,daring");
     printf("I am glad that I can do what I want at the moment");
     printf("Do you know how can I do it for your?");
-
-	
 }
